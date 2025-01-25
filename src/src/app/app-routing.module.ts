6b34325398f4e88b7cf40447cdba73c190222247
Rolf Fux <fux@hotmail.ch>
2025-01-25 08:47:37 +0100

    ng generate @angular/core:route-lazy-loading

--- src/src/app/app-routing.module.ts
+++ src/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormulaEditorComponent } from './components/formula-editor/formula-editor.component';
-import { FormulaEditorCreateNewComponent } from './components/formula-editor-create-new/formula-editor-create-new.component';
+
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'solver', pathMatch: 'full' },
-  { path: 'solver', component: FormulaEditorCreateNewComponent },
-  { path: 'solver/:gcTitle', component: FormulaEditorComponent },
+  { path: 'solver', loadComponent: () => import('./components/formula-editor-create-new/formula-editor-create-new.component').then(m => m.FormulaEditorCreateNewComponent) },
+  { path: 'solver/:gcTitle', loadComponent: () => import('./components/formula-editor/formula-editor.component').then(m => m.FormulaEditorComponent) },
 ];
 
 @NgModule({
